7ef97604b5a8e6971352e98f202d2315607c4e19
Raju kumar <rajusharmadata123@gmail.com>
2025-09-06 15:37:15 +0530

    add

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,26 @@
-import express from "express"
+import express from 'express';
 import { ENV } from '../config/env.js';
 import { router } from '../router/user.router.js';
-router
-
 
 const app = express();
 const PORT = ENV.PORT;
 
-app.use(express.json())
-app.use('/api/v1',router)
+app.use(express.json());
+app.use('/api/v1', router);
 
+// Test route
 app.get('/api/test', (req, res) => {
   res.status(200).json({
-   success:true,
-  })
-  app.get('/', (req, res) => {
-    res.send(200).json({
-      success:true,
-    })
-  })
-})
+    success: true,
+  });
+});
+
+// Root route
+app.get('/', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Backend is running 🚀',
+  });
+});
 
 export default app;
